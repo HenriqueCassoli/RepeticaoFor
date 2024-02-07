c673d430b0bf99ec4854fccd408cb5536bf28a0a
Cassoli <fernandes.cassoli@gmail.com>
2024-02-07 09:09:45 -0300

    first commit

---
+++ src/main/java/mypact/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mypact.repeticaofor;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+        for(int cc=0 ;cc<5;cc++){
+            System.out.println(cc);
+        }
+    }
+}
